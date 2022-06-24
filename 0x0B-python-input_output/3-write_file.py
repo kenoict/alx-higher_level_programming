e0cededf25676b531e9f17cbc3abc02a1aa2e3b9
Mafabi Kennedy <63707180+kenoict@users.noreply.github.com>
2022-06-24 15:29:56 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''Module for write_file method.'''
-
-
-def write_file(filename="", text=""):
-    '''Method for reading lines from file.'''
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)

27c23aeb4b92cb0f7823ae0561cccdb4e853ecdc
Mafabi Kennedy <63707180+kenoict@users.noreply.github.com>
2022-06-24 15:59:56 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// makes get request and prints status code
-const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  error && console.log(error);
-  response && console.log('code:', response.statusCode);
-});
--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// writes web page to file
-const fs = require('fs');
-const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
